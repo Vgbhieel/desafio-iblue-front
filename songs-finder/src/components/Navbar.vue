9941d96b5fbfe097924f8e7b3f6f057f0b9c1d57
Vitor Nascimento <bielvitor2008@hotmail.com>
2020-03-23 09:49:25 -0300

    Adicionado comentários no código fonte do componente NAVBAR.

--- songs-finder/src/components/Navbar.vue
+++ songs-finder/src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
     <nav>
         <v-app-bar dark>
+            
+            <!-- TÍTULO DA NAVBAR -->
             <v-toolbar-title>
                 <a href="/" style="text-decoration: none;">
                 <span class="font-weight-light white--text">Songs </span>
@@ -8,27 +10,43 @@
                 </a>
             </v-toolbar-title>
             <v-spacer></v-spacer>
+
+            <!-- BOTÕES DA NAVBAR - NÃO MOBILE -->
             <div class="hidden-xs-only">
+
+                <!-- BOTÃO HOME -->
                 <v-btn href="/" class="elevation-0">
                     <span>Home</span>
                     <v-icon right>mdi-home-outline</v-icon>
                 </v-btn>
+
+                <!-- BOTÃO PESQUISAR -->
                 <v-btn href="pesquisar" class="elevation-0">
                     <span>Pesquisar</span>
                     <v-icon right>mdi-magnify</v-icon>
                 </v-btn>
+
+                <!-- BOTÃO RELATÓRIO -->
                 <v-btn href="relatorio" class="elevation-0">
                     <span>Relatório</span>
                     <v-icon right>mdi-database-search</v-icon>
                 </v-btn>
             </div>
+
+            <!-- BOTÕES DA NAVBAR - MOBILE -->
             <div class="d-sm-none">
+
+                <!-- BOTÃO HOME -->
                 <v-btn href="/" class="elevation-0">
                     <v-icon center>mdi-home-outline</v-icon>
                 </v-btn>
+
+                <!-- BOTÃO PESQUISAR -->
                 <v-btn href="pesquisar" class="elevation-0">
                     <v-icon center>mdi-magnify</v-icon>
                 </v-btn>
+
+                <!-- BOTÃO RELATÓRIO -->
                 <v-btn href="relatorio" class="elevation-0 ">
                     <v-icon center>mdi-database-search</v-icon>
                 </v-btn>
